ce3fc4c0e236b64e4c53940235c51aca571fc44a
baligh8 <obaligh8@gmail.com>
2023-02-07 22:28:11 +0100

    change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Etudiant et1=new Etudiant("1","14523698","salhi","saleh",25,12,335,"ALINFO10");
+        et1.setClasse("ALINFO2");
+        System.out.println(et1.getClasse());
 
-        Personne p0 = new Personne("1","12121212","baligh" ,"othman" ,24);
-        p0.toString();
+        System.out.println("Hello world!");
+        Etudiant e1 = new Etudiant("1","09632949","moataz","trojette",15,15,100,"info2");
+        System.out.println(e1.getClasse());
 
-        System.out.println("Hello Baligh othman!");
+        Etudiant etudiant1 = new Etudiant("7000", "1001", "fadhel", "ben fadhel", 22,
+        12, 454545, "ALINFO10");
+        Etudiant etudiant2 = new Etudiant("7001", "1002", "amin", "ben amin", 23,
+                15, 454545, "ALINFO10");
+        Etudiant etudiant3 = new Etudiant("7002", "1003", "karim", "ben karim", 21,
+                11, 454545, "ALINFO10");
+
+        ArrayList<Etudiant> list=new ArrayList();
+        list.add(etudiant1);
+        list.add(etudiant2);
+        list.add(etudiant3);
+
+        Enum_Niveau e12 = Enum_Niveau.ALINFO1;
+        Enum_Niveau e2 = Enum_Niveau.ALINFO2;
+        Enum_Niveau e3 = Enum_Niveau.ALINFO3;
+        Enum_Niveau e4 = Enum_Niveau.ALINFO4;
+
+        System.out.println("The First Class is " + e1);
+        System.out.println("The Second Class is " + e2);
+        System.out.println("The Third Class is " + e3);
+        System.out.println("The Forth Class is " + e4);
+        System.out.println("");
+
+        Etudiant e15 = new Etudiant("1","09632949","moataz","trojette",15,15,100,"info2");
+        System.out.println(e1.getClasse());
+
+       
     }
 }
