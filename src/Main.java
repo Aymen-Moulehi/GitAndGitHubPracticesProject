62ba48e6d160cd6c622f872a973c4aa8fcf9a9db
Soulayma <soulaaymamansouri45@gmail.com>
2023-02-07 22:00:21 +0100

    adding Etudiant list to main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Etudiant etudiant1 = new Etudiant("7000", "1001", "fadhel", "ben fadhel", 22,
+        12, 454545, "ALINFO10");
+        Etudiant etudiant2 = new Etudiant("7001", "1002", "amin", "ben amin", 23,
+                15, 454545, "ALINFO10");
+        Etudiant etudiant3 = new Etudiant("7002", "1003", "karim", "ben karim", 21,
+                11, 454545, "ALINFO10");
+
+        ArrayList<Etudiant> list=new ArrayList();
+        list.add(etudiant1);
+        list.add(etudiant2);
+        list.add(etudiant3);
+
     }
 }
