bbd9699c207309acf324e48ae03f2100f71ae101
DELL <gherianimohamed10@gmail.com>
2023-02-07 21:55:07 +0100

    Adding in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Enum_Niveau e1 = Enum_Niveau.ALINFO1;
+        Enum_Niveau e2 = Enum_Niveau.ALINFO2;
+        Enum_Niveau e3 = Enum_Niveau.ALINFO3;
+        Enum_Niveau e4 = Enum_Niveau.ALINFO4;
+
+        System.out.println("The First Class is " + e1);
+        System.out.println("The Second Class is " + e2);
+        System.out.println("The Third Class is " + e3);
+        System.out.println("The Forth Class is " + e4);
+
     }
+
 }
