2295b5730659beebbefcdd83ffa87a7e325b149c
moham <nour.benahmed.6@gmail.com>
2023-02-07 21:57:21 +0100

    adding in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Etudiant et1=new Etudiant("1","14523698","salhi","saleh",25,12,335,"ALINFO10");
+        et1.setClasse("ALINFO2");
+
     }
 }
