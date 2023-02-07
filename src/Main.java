15fda57b70d061614029b92f9e763fcd0621fb5a
DELL <gherianimohamed10@gmail.com>
2023-02-07 22:22:25 +0100

    getting my code back

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,12 @@ public class Main {
         list.add(etudiant2);
         list.add(etudiant3);
 
-        Enum_Niveau e12 = Enum_Niveau.ALINFO1;
+        Enum_Niveau e11 = Enum_Niveau.ALINFO1;
         Enum_Niveau e2 = Enum_Niveau.ALINFO2;
         Enum_Niveau e3 = Enum_Niveau.ALINFO3;
         Enum_Niveau e4 = Enum_Niveau.ALINFO4;
 
-        System.out.println("The First Class is " + e1);
+        System.out.println("The First Class is " + e11);
         System.out.println("The Second Class is " + e2);
         System.out.println("The Third Class is " + e3);
         System.out.println("The Forth Class is " + e4);
