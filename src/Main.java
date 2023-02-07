d4f4561a9dd35c365754951c8f3d75bac03181e4
moataztrojette <moataztrojette@gmail.com>
2023-02-07 21:57:27 +0100

    PROMETHEE_1 :Classe Enseignant v3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Etudiant e1 = new Etudiant("1","09632949","moataz","trojette",15,15,100,"info2");
+        System.out.println(e1.getClasse());
     }
 }
